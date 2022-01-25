adb5dd5d640bc8066c1b431fe552fb93028357b1
Sainath Reddy <dukedustine123@gmail.com>
2022-01-25 18:23:08 +0530

    initial commit

---
+++ MyContactList_Daytona/app/src/main/java/com/masai/sainath/mycontactlist_daytona/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.masai.sainath.mycontactlist_daytona
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
