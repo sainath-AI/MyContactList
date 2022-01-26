69e4e01aa6f979443c2af4298922120b44f5eda9
Sainath Reddy <dukedustine123@gmail.com>
2022-01-26 13:47:00 +0530

    sample contact list for items layout testings

--- MyContactList_Daytona/app/src/main/java/com/masai/sainath/mycontactlist_daytona/ui/MainActivity.kt
+++ MyContactList_Daytona/app/src/main/java/com/masai/sainath/mycontactlist_daytona/ui/MainActivity.kt
@@ -1,13 +1,25 @@
 package com.masai.sainath.mycontactlist_daytona.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.masai.sainath.mycontactlist_daytona.R
+import com.masai.sainath.mycontactlist_daytona.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.NewContacts.setOnClickListener {
+            val intent = Intent(this,InsertContacts::class.java)
+            startActivity(intent)
+        }
+
+
 
     }
 }
